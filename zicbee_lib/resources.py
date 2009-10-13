c60a9b376ce7b9a0582c295a2ce5667b483d64a2
fab <fab@gnux.info>
2009-10-13 13:29:00 +0200

    try to fix resources

--- zicbee_lib/resources.py
+++ zicbee_lib/resources.py
@@ -1,11 +1,45 @@
 __all__ = ['get_players', 'resource_filename']
 
-import pkg_resources
+import os
+from pkg_resources import iter_entry_points, resource_filename as pkg_resource_filename, ExtractionError
+
+class _FakeEntryPoint(object):
+
+    def __init__(self, name, objname):
+        exec ('import %s as _mymod')
+        self._obj = getattr(_mymod, objname)
+        self.name = name
+
+    def load(self):
+        return self._obj
+
 def get_players():
     """ returns an iterable with available Player classes """
-    return pkg_resources.iter_entry_points("zicbee.player")
+    l =  list(iter_entry_points("zicbee.player"))
+    for p in l:
+        try:
+            p.load()
+        except:
+            l.remove(p)
+
+    if not l:
+        # failsafe
+        for mod in "zicbee_mplayer", "zicbee_vlc":
+            try:
+                l.append( _FakeEntryPoint(mod, 'Player') )
+            except ImportError:
+                pass
+    return l
 
 def resource_filename(package, resource):
     """ substitute to pkg_resources.resource_filename """
-    return pkg_resources.resource_filename(package, resource)
+    try:
+        return pkg_resource_filename(package, resource)
+    except (ExtractionError, KeyError):
+        fullpath = os.path.join(package.replace('.', os.sep), resource)
+        if os.path.exists(fullpath):
+            return os.path.abspath(fullpath)
+        else:
+            raise
+
 
