299c61c44f9abde5702f4b08a61a21902ca6f7c0
fab <fdev31@gmail.com>
2009-10-26 23:38:09 +0100

    now get command will save in download_dir variable

--- zicbee_lib/commands/command_get.py
+++ zicbee_lib/commands/command_get.py
@@ -1,6 +1,7 @@
 import os
 from zicbee_lib.downloader import Downloader
 from zicbee_lib.core import iter_webget, memory
+from zicbee_lib.config import config
 
 def get_last_search(output):
     uri = memory.get('last_search')
@@ -9,12 +10,13 @@ def get_last_search(output):
         return
 
     to_download = []
+    dst_prefix = config.download_dir
     for infos in iter_webget(memory['last_search']):
         uri, artist, album, title = infos.split(' | ', 4)
         ext = uri.rsplit('.', 1)[1].rsplit('?', 1)[0]
         if not os.path.exists(artist):
             os.mkdir(artist)
-        out_dir = os.path.join(artist, album)
+        out_dir = os.path.join(dst_prefix, artist, album)
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
         to_download.append((uri, os.path.join(out_dir, title) +"."+ext))
