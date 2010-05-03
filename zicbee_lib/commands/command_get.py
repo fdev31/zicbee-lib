b44aa923843155d4986c36547e2f4bd460de4e19
fab <fdevaux@wyplay.com>
2010-05-03 18:26:05 +0200

    fix get command (missing mkdir in some cases)

--- zicbee_lib/commands/command_get.py
+++ zicbee_lib/commands/command_get.py
@@ -15,11 +15,9 @@ def get_last_search(output):
     for infos in unroll(iter_webget(u) for u in memory['last_search']):
         uri, artist, album, title = infos.split(' | ', 4)
         ext = uri.rsplit('.', 1)[1].rsplit('?', 1)[0]
-        if not os.path.exists(artist):
-            os.mkdir(artist)
         out_dir = os.path.join(dst_prefix, artist, album)
         if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
+            os.makedirs(out_dir)
         to_download.append((uri, os.path.join(out_dir, title) +"."+ext))
     Downloader().run(to_download)
     output((e[1] for e in to_download))
